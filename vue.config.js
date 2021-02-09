4d7adde8fd4fb65ec3adb09e8e68f171a6a9001b
abd90 <53491426+abd90@users.noreply.github.com>
2021-02-09 13:27:49 +0300

    edit deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/onlineStoreDashboard/'
-      : '/'
+        ? '/onlineStore/'
+        : '/',
+  devServer: {
+    disableHostCheck: true,
+  },
+
+  transpileDependencies: ['vuetify'],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
 }
